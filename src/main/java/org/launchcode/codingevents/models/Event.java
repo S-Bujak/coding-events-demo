9d9584704278b69c35682610ec73017d6530932e
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-10-04 16:30:04 -0400

    Creating an AbstractEntity

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -2,19 +2,13 @@ package org.launchcode.codingevents.models;
 
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Objects;
+
 
 @Entity
-public class Event {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class Event extends AbstractEntity {
 
     @NotBlank(message="Name is required.")
     @Size(min=3, max=50, message="Name must be between 3 and 50 characters.")
@@ -55,9 +49,7 @@ public class Event {
         this.description = description;
     }
 
-    public int getId() {
-        return id;
-    }
+
 
     public String getContactEmail() {
         return contactEmail;
@@ -80,16 +72,5 @@ public class Event {
         return name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
